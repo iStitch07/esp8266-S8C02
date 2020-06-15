71628f11d3a4fdf50006966991845b408ca894c9
iStitch07 <iStitch07@gmail.com>
2020-06-15 20:54:36 +0300

    add abc request

--- S8-CO2.ino
+++ S8-CO2.ino
@@ -19,8 +19,12 @@ int s8_co2_mean2;
 float smoothing_factor = 0.5;
 float smoothing_factor2 = 0.15;
 
-byte cmd_s8[7]    = {0xFE, 0x44, 0x00, 0x08, 0x02, 0x9F, 0x25};
-byte response_s8[] = {0,0,0,0,0,0,0};
+byte cmd_s8[]       = {0xFE, 0x04, 0x00, 0x03, 0x00, 0x01, 0xD5, 0xC5};
+byte abc_s8[]       = {0xFE, 0x03, 0x00, 0x1F, 0x00, 0x01, 0xA1, 0xC3};
+byte response_s8[7] = {0, 0, 0, 0, 0, 0, 0};
+
+const int r_len = 7;
+const int c_len = 8;
 
 boolean wifi_reconnect() {
   Serial.printf("\nConnecting to %s ", ssid);
@@ -36,24 +40,24 @@ boolean wifi_reconnect() {
   return true;
 }
 
-void s8Request(byte packet[], int num) { 
+void s8Request(byte cmd[]) {
   s8Serial.begin(9600);
   while(!s8Serial.available()) {
-    s8Serial.write(cmd_s8,num); 
+    s8Serial.write(cmd, c_len); 
     delay(50); 
   }
   int timeout=0;
-  while(s8Serial.available() < num ) {
+  while(s8Serial.available() < r_len ) {
     timeout++; 
     if(timeout > 10) {
       while(s8Serial.available()) {
-        s8Serial.read(); 
+        s8Serial.read();
         break;
       }
     } 
     delay(50); 
   } 
-  for (int i=0; i < num; i++) { 
+  for (int i=0; i < r_len; i++) { 
     response_s8[i] = s8Serial.read(); 
   }
   
@@ -68,7 +72,7 @@ unsigned long s8Replay(byte rc_data[]) {
 }
 
 void co2_measure() {
-  s8Request(cmd_s8, 7);
+  s8Request(cmd_s8);
   s8_co2 = s8Replay(response_s8);
   
   if (!s8_co2_mean) s8_co2_mean = s8_co2;
@@ -81,6 +85,15 @@ void co2_measure() {
   return;
 }
 
+void get_abc() {
+  int abc_s8_time;
+  s8Request(abc_s8);
+  abc_s8_time = s8Replay(response_s8);
+
+  Serial.printf("ABC Time is: %d hours\n", abc_s8_time);
+  return;
+}
+
 void setup() {
   Serial.begin(115200);
   delay(10);
@@ -88,6 +101,8 @@ void setup() {
   if(WiFi.status() != WL_CONNECTED) {
     wifi_reconnect();
   }
+
+  get_abc();
 }
 
 void loop() {
